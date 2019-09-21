cfabae1580ba9d3a730039b4550ce14a58cc08e2
Leonel Jara <leoneljara3@gmail.com>
2019-09-20 21:15:04 -0400

    filled in evil objects, anime2D

--- Assets/Scripts/GameObjects/EvilObjects/EvilObjects.cs
+++ Assets/Scripts/GameObjects/EvilObjects/EvilObjects.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public abstract class EvilObjects : MonoBehaviour
 {
+    public bool canDie = true;
+    public bool canHurtPlayer = true;
+    public int health = 1;
+    public string[] hurtTags;
+
+    protected delegate void Damage();
+    protected event Damage OnDamage;
+    protected delegate void Dead();
+    protected event Dead OnDeath;
     // Start is called before the first frame update
     void Start()
     {
+        OnDamage += this.LoseHealth;
+        OnDeath += this.Despwan;
         
     }
 
@@ -15,14 +26,60 @@ public abstract class EvilObjects : MonoBehaviour
     {
         
     }
-
-    void Despwan()
+    protected void OnHitSelf()
     {
-
+        LoseHealth();
     }
 
-    void Dead()
+    protected void LoseHealth()
     {
-
+        health--;
+        CheckHealth();
     }
+
+    protected void CheckHealth()
+    {
+        if (health <= 0)
+        {
+            Despwan();
+        }
+    }
+
+    protected void Despwan()
+    {
+        Destroy(this.gameObject, 0);
+    }
+
+    protected bool CheckPlayerHit(Collision2D collision)
+    {
+        if (canHurtPlayer && collision.gameObject.tag.Equals("Player"))
+        {
+            GameManager.instance.player.Hit();
+            return true;
+        }
+        return false;
+    }
+    protected void CheckHitSelf(Collision2D collision)
+    {
+        if (canDie)
+        {
+            foreach (string tag in hurtTags)
+            {
+                if (collision.gameObject.tag.Equals(tag))
+                {
+                    OnHitSelf();
+                }
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!CheckPlayerHit(collision))
+        {
+            CheckHitSelf(collision);
+        }
+        
+        
+    }
+
 }
