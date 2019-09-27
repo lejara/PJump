8beaa74420cb8721a2289a5302149ddb8eb2d020
Leonel Jara <leoneljara3@gmail.com>
2019-09-27 01:42:44 -0400

    added evil beam

---
+++ Assets/Scripts/GameObjects/EvilObjects/EvilBeam/BeamHurt.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamHurt : MonoBehaviour
+{
+    public bool active = false;
+    public bool hasWarning = true;
+    public bool isShootingBeam = false;
+    public float activeTime = 1f;
+    public float warningTime = 1f;
+
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2D = GetComponent<Collider2D>();
+
+        collider2D.enabled = false;
+        spriteRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && !isShootingBeam)
+        {
+            if (hasWarning)
+            {
+                WarningShoot();
+            }
+            else
+            {
+                ShootBeam();
+            }
+            
+        }
+    }
+
+    void WarningShoot()
+    {
+        var color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, 0.2f);
+        spriteRenderer.enabled = true;
+        isShootingBeam = true;
+        StartCoroutine(BeamWarningWait());
+    }
+
+    void ShootBeam()
+    {
+        var color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, 0.8f);
+        isShootingBeam = true;
+        collider2D.enabled = true;
+        spriteRenderer.enabled = true;
+        StartCoroutine(BeamShutOffDelay());
+    }
+
+    void DeactivateBeam()
+    {
+        isShootingBeam = false;
+        collider2D.enabled = false;
+        spriteRenderer.enabled = false;
+        active = false;
+    }
+
+    IEnumerator BeamShutOffDelay()
+    {
+        yield return new WaitForSeconds(activeTime);
+        DeactivateBeam();
+    }
+
+    IEnumerator BeamWarningWait()
+    {
+        yield return new WaitForSeconds(warningTime);
+        ShootBeam();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            GameManager.instance.player.Hit();
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjects/EvilObjects/EvilBeam/BeamShooter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamShooter : EvilObjects
+{
+
+    public bool shootInOrder = false;
+    public bool shootAll = true;
+    public bool shootInInverals = true;
+
+    public float shootIntervalTime = 0.5f;
+    public BeamHurt[] beamHurts;
+
+    private CircleCollider2D circleCollider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    protected override void Activate()
+    {
+        isActive = true;
+        circleCollider2D.enabled = false;
+        CheckNextInterval();
+    }
+
+    void ShootAll()
+    {
+        if (isActive)
+        {
+            foreach (BeamHurt beam in beamHurts)
+            {
+                beam.active = true;
+            }
+
+            StartCoroutine(IntervalShootDelay());
+        }
+
+    }
+
+    void ShootInOrder()
+    {
+        //TODO: finish dis
+    }
+
+    void CheckNextInterval()
+    {
+        if (shootInOrder)
+        {
+            ShootInOrder();
+        }
+        else if (shootAll)
+        {
+            ShootAll();
+        }
+    }
+    IEnumerator IntervalShootDelay()
+    {
+        
+        yield return new WaitForSeconds(shootIntervalTime);
+        CheckNextInterval();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Activate();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
